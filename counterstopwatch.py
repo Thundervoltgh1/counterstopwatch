7ffd30207c4abaed979965e96867ffdf6e1f7479
Thundervoltgh1 <eniolusi@gmail.com>
2025-02-22 14:21:30 +0000

    Update counterstopwatch.py

--- counterstopwatch.py
+++ counterstopwatch.py
@@ -1,6 +1,6 @@
 from tkinter import*
-from time import*
-from time import sleep
+import time
+from tkinter import messagebox
 root=Tk()
 root.geometry("500x500")
 root.title("Stop watch")
@@ -10,21 +10,26 @@ min=StringVar()
 min.set("00")
 sec=StringVar()
 sec.set("00")
+j=0
 def g():
     try:
         j=int(hour.get())*3600+int(min.get())*60+int(sec.get())*1
     except:
         print("Please input the right value")
-    while j>-1:
+    while j>=-1:
         mins,secs= divmod(j,60)#divmod helps us to store quotient and remainder of the division
         hours=00
         if mins>60:
-            hours,min=divmod(min,60)
+            hours,mins=divmod(mins,60)
         hour.set("{00:2d}".format(hours))
         min.set("{00:2d}".format(mins))
         sec.set("{00:2d}".format(secs))
         root.update()
+
         time.sleep(1)
+        if (j==0):
+            messagebox.showinfo("timecountdown","times up!!!")
+        j-=1
 
 hourentry=Entry(root,width=3,font=("Arial",25,""),textvariable=hour)
 minentry=Entry(root,width=3,font=("Arial",25,""),textvariable=min)
